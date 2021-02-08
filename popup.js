b91e3e0f30d1084a13c8dfb3c9e641ebe43249d0
Tom Smithrosser <tom@tom>
2021-02-08 21:35:01 +0000

    add basic url catch

--- popup.js
+++ popup.js
@@ -1,5 +1,22 @@
-const body = document.getElementById('mainBody');
+document.addEventListener('DOMContentLoaded', function() {
 
-document.addEventListener('DOMContentLoaded', () => {
-	mainBody.innerHTML = "ready for all weather my g?";
+    var label = document.getElementById('label');
+    var status = document.getElementById('status');
+    var toggle = document.getElementById('toggle');
+
+    // get currently opened
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        let url = tabs[0].url;
+
+        let matches = url.match(/.*watch\?.*v=([^=&]*).*/);
+        if (matches) {
+            toggle.addEventListener('click', function() {
+                chrome.tabs.update(tabs[0].id, { url: "https://www.youtube.com/embed/" + matches[1] });
+            });
+        }
+        else {
+            status.innerHTML = '(probably) invalid url';
+        }
+
+    });
 });
